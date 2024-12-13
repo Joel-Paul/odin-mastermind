da1c5e14df6eebe588f2e631a7d1535048e8e3cb
Joel Paul <joel.paul@student.unsw.edu.au>
2024-12-13 16:04:46 +1100

    Implement player guessing

--- main.rb
+++ main.rb
@@ -1,40 +1,65 @@
+
+ROUNDS = 12
 MAX_DIGIT = 5
+MAX_LEN = 4
 
-CODE = '0123'
+# Generates a MAX_LEN digit code.
+# Allows trailing 0s and does not contain any digits over MAX_DIGIT
+def generate_code
+    code = ''
+    MAX_LEN.times {
+        code += rand(MAX_DIGIT + 1).to_s
+    }
+    puts "Code: #{code}"
+    code
+end
 
-def input_guess
+def input_code
     input = gets.chomp
-    until input_valid? input
-        puts 'Invalid code. Please try again:'
+    until code_valid? input
+        puts "Invalid code. Code must be #{MAX_LEN} digits in the range [#{0}, #{MAX_DIGIT}]. Please try again:"
         input = gets.chomp
     end
     input
 end
 
-def input_valid?(input)
-    input.length == 4 && input.split('').all? { |digit| digit.to_i.between?(0, MAX_DIGIT) }
+def code_valid?(code)
+    code.length == MAX_LEN && code.split('').all? { |digit| digit.to_i.between?(0, MAX_DIGIT) }
 end
 
-def guess_feedback(guess, code)
+def guess_feedback(guess, answer)
+    # Map to keep track of which digits have been guessed, otherwise partial guesses can double up
+    digits_map = answer.split('').reduce(Hash.new(0)) do |digits_map, digit|
+        digits_map[digit] += 1
+        digits_map
+    end
+
     feedback = {exact: 0, partial: 0}
-    guess.each_with_index do |i, g|
-        if code[i] == g
+    
+    guess.split("").each_with_index do |g, i|
+        if answer[i] == g
             feedback[:exact] += 1
-        elsif code.include? g
+            digits_map[g] -= 1
+        elsif digits_map.has_key? g and digits_map[g] > 0
             feedback[:partial] += 1
+            digits_map[g] -= 1
         end
     end
     feedback
 end
 
+code = generate_code
+
 guess = ''
-12.times do |round|
-    puts "Round #{round}:"
-    guess = input_guess
-    break if guess == CODE
+ROUNDS.times do |round|
+    puts "Round #{round + 1}:"
+    guess = input_code
+    puts guess_feedback(guess, code)
+    puts
+    break if guess == code
 end
 
-if guess == CODE
+if guess == code
     puts 'You wins!'
 else
     puts 'You loses!'
